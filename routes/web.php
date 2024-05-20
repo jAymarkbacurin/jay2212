fbbeba45023e043b0db2985386ece478d4418ce2
awda@gma <awda@gma>
2024-05-20 16:50:11 +0800

    SSXCZ

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/deleteuser', [ProfileController::class, 'delete'])->name('deleteuser');
+    Route::get('/Edituser', [ProfileController::class, 'edituser'])->name('Edituser');
+    Route::post('/Editpost', [ProfileController::class, 'editpost'])->name('Editpost');
+
+});
+
+require __DIR__.'/auth.php';
